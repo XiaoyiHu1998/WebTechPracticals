814823e4bcf2e027a4f08cae3eee72d1af3a38a7
LucasDissie <l.k.disseldorp@students.uu.nl>
2022-03-17 11:14:33 +0100

    Menu's are generated fully

--- P2/scripts/headerMenu.js
+++ P2/scripts/headerMenu.js
@@ -1,22 +1,66 @@
 var selectedHtmlTag;
-var htmlForm;
+var htmlSelector;
+var fontInput;
+var colorInput;
 var htmlNodes;
 
-function createHeader*
-Menus(){
-    htmlNodes = getElementsByTagNames(["body", "header", "footer", "aside", "article", "section"]);
+function createHeaderMenus(){
+    let ul = document.getElementsByTagName("ul")[0];
+    ul.appendChild(createHtmlSelector());
+    ul.appendChild(createFontMenu());
+    ul.appendChild(createColorMenu());
     
-    htmlForm = document.getElementById("htmlselector");
-    var htmlOptions = htmlNodes.map(x => document.createElement('option'));
-    for (let i = 0; i < htmlOptions.length; i++) {
-        htmlOptions[i].setAttribute("value", i);
-        htmlOptions[i].setAttribute("selected", "selected");
-        htmlOptions[i].appendChild(document.createTextNode(htmlNodes[i].nodeName));
-        htmlForm.appendChild(htmlOptions[i]);
+    function createHtmlSelector () {
+        //creating
+        let li = document.createElement("li");
+        let form = document.createElement("form");
+        form.setAttribute("name", "selector");
+        let text = document.createTextNode("Website part ");
+        htmlSelector = document.createElement("select");
+        htmlSelector.setAttribute("id", "htmlselector");
+        form.appendChild(text);
+        form.appendChild(htmlSelector);
+        li.appendChild(form);
+        //creating
+        //populating
+        htmlNodes = getElementsByTagNames(["body", "header", "footer", "aside", "article", "section"]);
+    
+        var htmlOptions = htmlNodes.map(x => document.createElement('option'));
+        for (let i = 0; i < htmlOptions.length; i++) {
+            htmlOptions[i].setAttribute("value", i);
+            htmlOptions[i].setAttribute("selected", "selected");
+            htmlOptions[i].appendChild(document.createTextNode(htmlNodes[i].nodeName));
+            htmlSelector.appendChild(htmlOptions[i]);
+        }
+        //populating
+        return li;
     }
-    
-    function createHeader (params) {
-        
+    function createFontMenu() {
+        let li = document.createElement("li");
+        let form = document.createElement("form");
+        form.setAttribute("name", "fontsize");
+        let text = document.createTextNode("Fontsize ");
+        fontInput = document.createElement("input");
+        fontInput.setAttribute("type", "number");
+        fontInput.setAttribute("min", 1);
+        fontInput.setAttribute("id", "fontinput");
+        form.appendChild(text);
+        form.appendChild(fontInput);
+        li.appendChild(form);
+        return li;
+    }
+    function createColorMenu() {
+        let li = document.createElement("li");
+        let form = document.createElement("form");
+        form.setAttribute("name", "color");
+        let text = document.createTextNode("Color ");
+        colorInput = document.createElement("input");
+        colorInput.setAttribute("type", "color");
+        colorInput.setAttribute("id", "colorinput");
+        form.appendChild(text);
+        form.appendChild(colorInput);
+        li.appendChild(form);
+        return li;
     }
 
 }
@@ -30,17 +74,14 @@ function changeFontSize(){
 }
 
 function changeSelectedTag(){
-    var value = htmlForm.options[htmlForm.selectedIndex].value;
+    var value = htmlSelector.options[htmlSelector.selectedIndex].value;
     selectedHtmlTag = htmlNodes[value];
 }
 
 function registerEvents(){
-    var fontInput = document.getElementById("fontinput");
-    var colorInput = document.getElementById("colorinput");
-    var tagInput = document.getElementById("htmlselector");
     fontInput.addEventListener("input", changeFontSize);
     colorInput.addEventListener("input", changeColor);
-    tagInput.addEventListener("change", changeSelectedTag);
+    htmlSelector.addEventListener("change", changeSelectedTag);
 }
 
 function getElementsByTagNames(names){
@@ -50,5 +91,5 @@ function getElementsByTagNames(names){
     }
     return elements
 }
+createHeaderMenus();
 registerEvents();
-populateHtmlMenu();
